189289886d26dcd04f62ba0272eb8aa6d493e066
ПК <kyaacdc2@gmail.com>
2026-01-01 22:11:16 +0000

    аа

--- reverse_line/reverse_line.cpp
+++ reverse_line/reverse_line.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <sstream>
 
 using namespace std;
 
@@ -10,15 +11,18 @@ int main() {
     ofstream output("output.txt");
 
     string text;
-    getline(input, text);      
+    getline(input, text);              
 
-    reverse(text.begin(), text.end()); 
+    reverse(text.begin(), text.end());
 
-    int words = 1;
-    for (char c : text)
-        if (c == ' ') words++;
+    stringstream ss(text);
+    string word;
+    int words = 0;
 
-    output << text;               
+    while (ss >> word)                
+        words++;
+
+    output << text;
 
     cout << "Text: " << text << endl;
     cout << "Words: " << words << endl;
