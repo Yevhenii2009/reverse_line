63b049f1aedc26d58ae8b797fe0edcfd4e1f7175
ПК <kyaacdc2@gmail.com>
2026-01-01 16:53:15 +0000

    Add project files.

---
+++ reverse_line/reverse_line.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+
+    string text;
+    getline(input, text);      
+
+    reverse(text.begin(), text.end()); 
+
+    int words = 1;
+    for (char c : text)
+        if (c == ' ') words++;
+
+    output << text;               
+
+    cout << "Text: " << text << endl;
+    cout << "Words: " << words << endl;
+
+    input.close();
+    output.close();
+
+    return 0;
+}
